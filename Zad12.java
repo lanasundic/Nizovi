46402e3b5276e646fa85db23219d6b1cdb40c534
Lana Sundic <lanasundic@gmail.com>
2025-03-24 21:51:15 +0100

    fix exercise 12

--- Zad12.java
+++ Zad12.java
@@ -6,10 +6,28 @@ import java.util.Scanner;
 public class Zad12 {
 
     public static int[] BrojeviKojiSuManjiIliJednakisaN(int a[], int n) {
-        for (int i = 0; i < a.length; i++) {
-            
+        int[] b = new int[n];
+
+        boolean Postoji = false;
+
+        int missingCounter = 0;
+
+        for (int j = 1; j <= n; j++) {  //provjeravam da li postoje ili ne
+            Postoji = false;
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] == j) { //j je prirodni broj kao
+                    Postoji = true;
+                    break;
+                }
+            }
+            if (!Postoji) {
+                b[missingCounter++] = j;
+            }
         }
-    } 
+
+        return b;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -22,5 +40,11 @@ public class Zad12 {
             a[i] = scanner.nextInt();
         }
 
+        int[] fale = BrojeviKojiSuManjiIliJednakisaN(a, n);
+
+        for (int i = 0; i < fale.length; i++) {
+            if (fale[i] != 0)
+                System.out.print(fale[i] + " ");
+        }
     }
 }
